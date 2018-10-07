385ac57dc3a889c995c1d95519f3ee3e35bfde26
Kévin Petit <kpet@free.fr>
2018-10-07 19:54:38 +0100

    Add basic Travis CI support for Linux and OSX
    Signed-off-by: Kévin Petit <kpet@free.fr>

---
+++ tests/travis/step-install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ex
+
+if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
+    sudo apt-get install -qq g++-7
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 90
+fi
+
+if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
+    brew install ccache bash
+    export PATH="/usr/local/opt/ccache/libexec:$PATH"
+fi
+
+if [ "${TRAVIS_OS_NAME}" == "windows" ]; then
+    choco install make
+fi
+
+set +ex
---
+++ tests/travis/step-script.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -ex
+
+if [ "${TRAVIS_OS_NAME}" != "windows" ]; then
+    ccache --max-size=2G
+    ccache --show-stats
+fi
+
+declare -A CMD_TIMEOUT_MAP
+CMD_TIMEOUT_MAP[osx]=gtimeout
+CMD_TIMEOUT_MAP[linux]=timeout
+CMD_TIMEOUT="${CMD_TIMEOUT_MAP[${TRAVIS_OS_NAME}]}"
+
+declare -A BUILD_TIMEOUT_MAP
+BUILD_TIMEOUT_MAP[osx]=2000
+BUILD_TIMEOUT_MAP[linux]=2400
+BUILD_TIMEOUT="${BUILD_TIMEOUT_MAP[${TRAVIS_OS_NAME}]}"
+
+BUILD_DIR=build
+
+mkdir "${BUILD_DIR}"
+cd "${BUILD_DIR}"
+
+cmake --version
+cmake -DCMAKE_BUILD_TYPE=Release -DVULKAN_IMPLEMENTATION=talvos -DSPIRV_WERROR=OFF ..
+
+set +e
+${CMD_TIMEOUT} ${BUILD_TIMEOUT} make -j2
+BUILD_STATUS=$?
+set -e
+
+if [ "${TRAVIS_OS_NAME}" != "windows" ]; then
+    ccache --show-stats
+fi
+
+if [ ${BUILD_STATUS} -ne 0 ]; then
+    exit ${BUILD_STATUS}
+fi
+
+./simple_test
