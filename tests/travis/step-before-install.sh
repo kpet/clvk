cdb5a784d22796c6cb5d2ad0fd259a1d9299726f
Kévin Petit <kpet@free.fr>
2020-01-04 12:08:16 +0000

    Remove the need for Clang 10 for code formatting (#139)
    Signed-off-by: Kévin Petit <kpet@free.fr>

--- tests/travis/step-before-install.sh
+++ tests/travis/step-before-install.sh
@@ -4,7 +4,5 @@ set -ex
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-    sudo add-apt-repository -y 'deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial main'
-    wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
     sudo apt-get update -qq
 fi
