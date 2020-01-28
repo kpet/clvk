705b7a3e847326fa3a6955236b83348671986855
James Price <jrprice@google.com>
2020-01-28 00:25:23 -0500

    Add Travis CI coverage for the ICD interface

--- tests/travis/step-install.sh
+++ tests/travis/step-install.sh
@@ -2,6 +2,11 @@
 
 set -ex
 
+if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
+    sudo apt update
+    sudo apt install ocl-icd-libopencl1
+fi
+
 if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
     brew install ccache bash
     export PATH="/usr/local/opt/ccache/libexec:$PATH"
