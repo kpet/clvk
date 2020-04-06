c5cd0c03bbdc2ba4435bdd35812f5184ef81c00b
Kévin Petit <kpet@free.fr>
2020-04-07 00:27:41 +0100

    WIP CTS in CI
    Change-Id: Iaa9376c2257d7b4312179c9d7a9ccf4f45a4436d

--- tests/conformance/run-conformance.py
+++ tests/conformance/run-conformance.py
@@ -28,7 +28,6 @@ import tempfile
 
 THIS_DIR = os.path.dirname(__file__)
 TOP_DIR = os.path.realpath(os.path.join(THIS_DIR, '..', '..'))
-CTS_BUILD_DIR = os.path.join(TOP_DIR, 'build', 'conformance')
 CTS_DIR = os.path.join(TOP_DIR, 'external', 'OpenCL-CTS')
 
 # ('Name', 'binary', 'arg0', 'arg1', ...)
@@ -40,11 +39,10 @@ TESTS_QUICK = (
     ('Compiler', 'compiler/test_compiler'),
     ('Contractions', 'contractions/test_contractions'),
     ('Device Partitioning', 'device_partition/test_device_partition'),
+    ('Device timer', 'device_timer/test_device_timer'),
     ('Events', 'events/test_events'),
     ('Geometric Functions', 'geometrics/test_geometrics'),
     ('Mem (Host Flags)', 'mem_host_flags/test_mem_host_flags'),
-    ('Multiple Device/Context', 'multiple_device_context/test_multiples'),
-    ('Printf', 'printf/test_printf'),
     ('Profiling', 'profiling/test_profiling'),
 )
 
@@ -74,10 +72,12 @@ TESTS_FOR_WIMPY = TESTS_QUICK + (
     ('Vectors', 'vectors/test_vectors'),
     ('C11 Atomics', 'c11_atomics/test_c11_atomics'),
     ('Device execution', 'device_execution/test_device_execution'),
-    ('Device timer', 'device_timer/test_device_timer'),
     ('Generic Address Space', 'generic_address_space/test_generic_address_space'),
+    ('Half Ops', 'half/test_half'),
+    ('Multiple Device/Context', 'multiple_device_context/test_multiples'),
     ('Non-uniform work-group', 'non_uniform_work_group/test_non_uniform_work_group'),
     ('Pipes', 'pipes/test_pipes'),
+    ('Printf', 'printf/test_printf'),
     ('SPIR', 'spir/test_spir'),
     ('SPIR-V', 'spirv_new/test_spirv_new', '--spirv-binaries-path', os.path.join(CTS_DIR, 'test_conformance', 'spirv_new', 'spirv_bin')),
     ('SVM', 'SVM/test_svm'),
@@ -165,7 +165,7 @@ def load_json(path):
     with open(path) as f:
         return json.load(f)
 
-def run_conformance_binary(path, args):
+def run_conformance_binary(path, testargs, args):
     start = datetime.datetime.utcnow()
     dirname = os.path.dirname(path)
     binary = os.path.basename(path)
@@ -173,8 +173,19 @@ def run_conformance_binary(path, args):
     workdir = os.path.dirname(path)
     result_json = os.path.join(workdir, 'conf.json')
     os.environ['CL_CONFORMANCE_RESULTS_FILENAME'] = result_json
+    if args.vulkan_loader_json_path:
+        print("Using ICD JSON: " + args.vulkan_loader_json_path)
+        os.environ['VK_ICD_FILENAMES'] = args.vulkan_loader_json_path
+    if os.name == 'nt':
+        path += '.exe'
+    print(path)
+    os.environ['VK_LOADER_DEBUG'] = 'all'
+    import pprint
+    pprint.pprint(os.environ)
+    os.environ['LD_LIBRARY_PATH'] = args.opencl_lib_dir
+    os.environ['DYLD_LIBRARY_PATH'] = args.opencl_lib_dir
     p = subprocess.Popen(
-        [path] + args,
+        [path] + testargs,
         stdout=subprocess.PIPE, stderr=subprocess.PIPE,
         cwd=workdir
     )
@@ -182,7 +193,8 @@ def run_conformance_binary(path, args):
     stdout = stdout.decode('utf-8')
     stderr = stderr.decode('utf-8')
     end = datetime.datetime.utcnow()
-    #print(stdout)
+    print(stderr)
+    print(stdout)
     duration = end - start
 
     has_results = False
@@ -266,7 +278,8 @@ def run_tests(args):
         if args.filter and not re.match(args.filter, name):
             continue
         print("Running", name, "...")
-        status = run_conformance_binary(os.path.join(CTS_BUILD_DIR, os.path.basename(binary)), list(test_args))
+        abspath = os.path.join(args.cts_install_dir, os.path.basename(binary))
+        status = run_conformance_binary(abspath, list(test_args), args)
         results[name] = status
         totals = get_suite_totals(status)
         print("Done, retcode = %d [%s]." % (status['retcode'], status['duration']))
@@ -278,6 +291,7 @@ def run_tests(args):
 def check_reference(results, reference, args):
     print("")
     print("Difference w.r.t. reference results:")
+    has_diff = False
     for name in sorted(results):
         # Skip checking if there is no reference
         if not name in reference:
@@ -320,9 +334,15 @@ def check_reference(results, reference, args):
 
         # Print differences if any
         if msgs:
+            has_diff = True
             print("\t{}".format(name))
             for msg in msgs:
                 print("\t\t{}".format(msg))
+    if has_diff:
+        return False
+    else:
+        print("\tNone.")
+        return True
 
 def report(results, args):
     total = 0
@@ -389,9 +409,11 @@ def report(results, args):
     print(colour + line + COLOUR_RESET)
     print("")
 
+    success = True
     if args.reference_results:
         reference = load_json(args.reference_results)
-        check_reference(results, reference['test-results'], args)
+        success = check_reference(results, reference['test-results'], args)
+    return success
 
 def main():
 
@@ -422,6 +444,22 @@ def main():
         '--compare-only', action='store_true',
         help="Only compare results to reference",
     )
+    parser.add_argument(
+        '--vulkan-loader-json-path', default=None,
+        help='Path to Vulkan Loader ICD JSON',
+    )
+
+    parser.add_argument(
+        '--cts-install-dir',
+        default=os.path.join(TOP_DIR, 'build', 'conformance'),
+        help='Path to installed CTS',
+    )
+    
+    parser.add_argument(
+        '--opencl-lib-dir',
+        default=os.path.join(TOP_DIR, 'build'),
+        help='Path to OpenCL library',
+    )
 
     parser.add_argument(
         '--filter',
@@ -448,7 +486,8 @@ def main():
         results = load_json(args.save_results)
 
     # Process results
-    report(results['test-results'], args)
+    if not report(results['test-results'], args):
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
