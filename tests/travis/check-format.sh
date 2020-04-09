6631ee4ca4155a114fbffc05df841a9163534496
Kévin Petit <kpet@free.fr>
2020-04-09 15:25:18 +0100

    Add a separate CI stage to check code formatting
    Signed-off-by: Kévin Petit <kpet@free.fr>

--- tests/travis/check-format.sh
+++ tests/travis/check-format.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+GIT_CLANG_FORMAT=${GIT_CLANG_FORMAT:-git-clang-format}
+
 # Run git-clang-format to check for violations
 CLANG_FORMAT_OUTPUT=/tmp/clvk-clang-format-output.txt
-git-clang-format --diff origin/master --extensions cpp,hpp >$CLANG_FORMAT_OUTPUT
+${GIT_CLANG_FORMAT} --diff origin/master --extensions cpp,hpp >$CLANG_FORMAT_OUTPUT
 
 # Check for no-ops
 grep '^no modified files to format$' "$CLANG_FORMAT_OUTPUT" && exit 0
