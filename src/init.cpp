6daebbeec42239e29c489354a606f56aa7ffec67
Kévin Petit <kpet@free.fr>
2021-02-02 16:54:38 +0000

    Fix building for Armv7/Android

--- src/init.cpp
+++ src/init.cpp
@@ -33,7 +33,7 @@ std::string gLLVMSPIRVPath = DEFAULT_LLVMSPIRV_BINARY_PATH;
 #endif
 std::string gCLSPVOptions;
 
-static VkBool32 VKAPI_CALL debugCallback(
+static VkBool32 VKAPI_PTR debugCallback(
     VkDebugReportFlagsEXT flags, VkDebugReportObjectTypeEXT objectType,
     uint64_t object, size_t location, int32_t messageCode,
     const char* pLayerPrefix, const char* pMessage, void* pUserData) {
@@ -168,7 +168,7 @@ void clvk_global_state::init_vulkan() {
             VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT |
             VK_DEBUG_REPORT_INFORMATION_BIT_EXT |
             VK_DEBUG_REPORT_WARNING_BIT_EXT,
-        debugCallback, // pfnCallback
+        &debugCallback, // pfnCallback
         NULL           // pUserData
     };
 
