e6cd6d084fa8d553592484b06fb8da4d958cbe53
Romaric Jodin <rjodin@chromium.org>
2022-03-07 20:07:20 +0100

    fix wrong is_valid_device usage

--- src/device.cpp
+++ src/device.cpp
@@ -26,7 +26,7 @@
 extern "C" void CL_API_CALL clvk_override_vklimits(cl_device_id device,
                                                    uint32_t x, uint32_t y,
                                                    uint32_t z) {
-    assert(is_valid_device(device));
+    assert(device != nullptr && icd_downcast(device)->is_valid());
     icd_downcast(device)->set_maxComputeWorkGroupCount(x, y, z);
 }
 
