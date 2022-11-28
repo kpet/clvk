0aa9775b8fef228937ef536315fca616aa51a0db
Romaric Jodin <rjodin@chromium.org>
2022-11-28 10:11:37 +0100

    use default value from config.def

--- src/device_properties.hpp
+++ src/device_properties.hpp
@@ -17,16 +17,25 @@
 #include <memory>
 #include <string>
 
+#include "config.hpp"
 #include "cl_headers.hpp"
 
 struct cvk_device_properties {
     virtual cl_ulong get_global_mem_cache_size() const { return 0; }
     virtual cl_ulong get_num_compute_units() const { return 1; }
 
-    virtual cl_uint get_max_cmd_batch_size() const { return 10000; }
-    virtual cl_uint get_max_first_cmd_batch_size() const { return 10000; }
-    virtual cl_uint get_max_cmd_group_size() const { return UINT32_MAX; }
-    virtual cl_uint get_max_first_cmd_group_size() const { return UINT32_MAX; }
+    virtual cl_uint get_max_cmd_batch_size() const {
+        return config.max_cmd_batch_size();
+    }
+    virtual cl_uint get_max_first_cmd_batch_size() const {
+        return config.max_first_cmd_batch_size();
+    }
+    virtual cl_uint get_max_cmd_group_size() const {
+        return config.max_cmd_group_size();
+    }
+    virtual cl_uint get_max_first_cmd_group_size() const {
+        return config.max_first_cmd_group_size();
+    }
 
     virtual std::string get_compile_options() const { return ""; }
 };
