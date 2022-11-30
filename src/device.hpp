02ecc81f44212716aa5b03acb09499318da579af
Kévin Petit <kpet@free.fr>
2022-11-30 19:31:01 +0000

    Update src/device.hpp

--- src/device.hpp
+++ src/device.hpp
@@ -275,7 +275,6 @@ struct cvk_device : public _cl_device_id,
                is_vulkan_extension_enabled(
                    VK_EXT_DESCRIPTOR_INDEXING_EXTENSION_NAME);
     }
-    
     bool supports_atomic_order_acq_rel() const {
         return m_features_vulkan_memory_model.vulkanMemoryModel;
     }
