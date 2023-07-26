f13fc41c9b41bf0a98b88e752b66a3ea659ed9e5
Romaric Jodin <rjodin@chromium.org>
2023-07-26 09:49:03 +0200

    fix clang-format usage
    Since we use InsertBraces, we need to use at least clang-format-15.
    But only installing clang-format-15, does not pull git-clang-format
    with it.
    So let's install both clang-format and clang-format-15, and force it
    to use clang-format-15 to ensure that InsertBraces is supported

--- tests/check-format.sh
+++ tests/check-format.sh
@@ -2,9 +2,14 @@
 
 GIT_CLANG_FORMAT=${GIT_CLANG_FORMAT:-git-clang-format}
 
+if [ ${CLANG_FORMAT_BINARY} ]; then
+   GIT_CLANG_FORMAT_BINARY="--binary ${CLANG_FORMAT_BINARY}"
+fi
+GIT_CLANG_FORMAT_BINARY=${GIT_CLANG_FORMAT_BINARY:-}
+
 # Run git-clang-format to check for violations
 CLANG_FORMAT_OUTPUT=/tmp/clvk-clang-format-output.txt
-${GIT_CLANG_FORMAT} --diff origin/main --extensions cpp,hpp >$CLANG_FORMAT_OUTPUT
+${GIT_CLANG_FORMAT} ${GIT_CLANG_FORMAT_BINARY} --diff origin/main --extensions cpp,hpp >$CLANG_FORMAT_OUTPUT
 
 # Check for no-ops
 grep '^no modified files to format$' "$CLANG_FORMAT_OUTPUT" && exit 0
