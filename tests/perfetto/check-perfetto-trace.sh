692c3682cc1516d3bc0753da3c790af52efc334f
Romaric Jodin <rjodin@chromium.org>
2024-01-15 18:51:08 +0100

    fix check-perfetto-trace.sh

--- tests/perfetto/check-perfetto-trace.sh
+++ tests/perfetto/check-perfetto-trace.sh
@@ -7,7 +7,7 @@ set -xe
 SCRIPT_DIR="$(dirname $(realpath "${BASH_SOURCE[0]}"))"
 TRACE_FILE="$1"
 OUTPUT_FILE="${SCRIPT_DIR}/output.txt"
-EXPECTATION_SORTED=${SCRIPT_DIR}/expectation-sorted.txt
+EXPECTATION_SORTED="${SCRIPT_DIR}/expectation-sorted.txt"
 
 # Either it is in your path, or you need to define the environment variable
 TRACE_PROCESSOR_SHELL=${TRACE_PROCESSOR_SHELL:-"trace_processor_shell"}
@@ -21,6 +21,6 @@ echo "SELECT name FROM slice WHERE slice.category='clvk'" \
 # Also sort the expectation to make sure to apply the same sort algorithm to the output and the expectation.
 sort "${SCRIPT_DIR}/expectation.txt" > "${EXPECTATION_SORTED}"
 
-diff "${OUTPUT_FILE}" "${SCRIPT_DIR}/expectation.txt" "{EXPECTATION_SORTED}"
+diff "${OUTPUT_FILE}" "${SCRIPT_DIR}/expectation.txt" "${EXPECTATION_SORTED}"
 
 rm "${OUTPUT_FILE}" "${EXPECTATION_SORTED}"
