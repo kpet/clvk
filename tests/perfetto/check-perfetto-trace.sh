eadfb00559f83aa5bf60e0dc4ef8d827bccf1291
Romaric Jodin <89833130+rjodinchr@users.noreply.github.com>
2024-01-16 19:05:59 +0100

    add test using perfetto in CI (#650)
    * add test using perfetto in CI
    
    fix #442
    
    * Update .github/workflows/presubmit.yml
    
    Co-authored-by: Kévin Petit <kpet@free.fr>
    
    * add missing part of setting perfettodir
    
    * Update presubmit.yml
    
    * also sort the expectation to make sure to apply the same sort algo
    
    * fix check-perfetto-trace.sh
    
    * Update check-perfetto-trace.sh
    
    * add expectation file as a parameter of perfetto check script
    
    ---------
    
    Co-authored-by: Kévin Petit <kpet@free.fr>

---
+++ tests/perfetto/check-perfetto-trace.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -xe
+
+
+[[ $# -eq 2 ]] || (echo "[ERROR] USAGE: ${BASH_SOURCE[0]} <input_trace> <expectation_file>" && exit -1)
+
+SCRIPT_DIR="$(dirname $(realpath "${BASH_SOURCE[0]}"))"
+TRACE_FILE="$1"
+EXPECTATION_FILE="$2"
+OUTPUT_FILE="${SCRIPT_DIR}/output.txt"
+EXPECTATION_SORTED="${EXPECTATION_FILE}.sorted"
+
+# Either it is in your path, or you need to define the environment variable
+TRACE_PROCESSOR_SHELL=${TRACE_PROCESSOR_SHELL:-"trace_processor_shell"}
+
+echo "SELECT name FROM slice WHERE slice.category='clvk'" \
+    | "${TRACE_PROCESSOR_SHELL}" -q /dev/stdin "${TRACE_FILE}" \
+    | sort \
+    | uniq \
+          > "${OUTPUT_FILE}"
+
+# Also sort the expectation to make sure to apply the same sort algorithm to the output and the expectation.
+sort "${EXPECTATION_FILE}" > "${EXPECTATION_SORTED}"
+
+diff "${OUTPUT_FILE}" "${EXPECTATION_SORTED}"
+rm "${OUTPUT_FILE}" "${EXPECTATION_SORTED}"
