d278fa0ab0c5801043966674346cd3856653bc12
Romaric Jodin <rjodin@google.com>
2024-11-26 21:16:45 +0100

    do not copy config files to build directory
    Add the path to the config files in the sources instead.
    
    Ref #743

--- tests/config/run_test.sh
+++ tests/config/run_test.sh
@@ -2,27 +2,29 @@
 
 set -xe
 
-if [ $# -eq 0 ]; then
-  echo "Error: Please append path to the test binary path as a command-line argument."
+if [ $# -ne 2 ]; then
+  echo "ERROR: USAGE: ${0} <config_test-path> <config_files-path>"
   exit 1
 fi
 
-TEMP_DIR="$(mktemp -d)"
-pushd "${TEMP_DIR}"
+# Assign the base path from the argument
+binary_path="$(realpath $1)"
+config_files_path=$2
 
+# Make temporary directory
+TEMP_DIR="$(mktemp -d)"
 function clean() {
     rm -r "${TEMP_DIR}"
 }
 trap clean EXIT
 
-# Assign the base path from the argument
-binary_path="$1"
-
-cp "${binary_path}/clvk.conf" "${TEMP_DIR}"
+# Copy assets to temporary directory
+cp "${config_files_path}/conf_test.conf" "${TEMP_DIR}"
+cp "${config_files_path}/clvk.conf" "${TEMP_DIR}"
 
 # Run test
-CLVK_CONFIG_FILE="${binary_path}/conf_test.conf" \
+pushd "${TEMP_DIR}"
+CLVK_CONFIG_FILE="${TEMP_DIR}/conf_test.conf" \
 CLVK_LOG_COLOUR=1 \
 "${binary_path}/config_test"
-
 popd
