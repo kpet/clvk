a79a2e9222e5c562598e7ef8ee91a53c0d2a3bd9
Paulius Velesko <pvelesko@pglc.io>
2026-02-16 15:11:01 +0200

    cl_ext_buffer_device_address: rename device_to_buffer_map, make private

--- src/context.hpp
+++ src/context.hpp
@@ -14,10 +14,10 @@
 
 #pragma once
 
+#include "cl_headers.hpp"
 #include "device.hpp"
 #include "objects.hpp"
 #include "unit.hpp"
-#include "cl_headers.hpp"
 #include <unordered_map>
 
 using cvk_printf_callback_t = void(CL_CALLBACK*)(const char* buffer, size_t len,
@@ -125,12 +125,15 @@ struct cvk_context : public _cl_context,
     cvk_command_queue* get_or_create_image_init_command_queue();
     void free_image_init_command_queue();
 
-    // Map for storing device pointers to buffer pointers
-    // Support cl_ext_buffer_device_address
-    std::unordered_map<cl_mem_device_address_ext, cvk_buffer*> device_to_buffer_map;
+    std::unordered_map<cl_mem_device_address_ext, cvk_buffer*>&
+    device_to_buffer_map() {
+        return m_device_to_buffer_map;
+    }
 
 private:
     cvk_device* m_device;
+    std::unordered_map<cl_mem_device_address_ext, cvk_buffer*>
+        m_device_to_buffer_map;
     std::mutex m_callbacks_lock;
     std::vector<cvk_context_callback> m_destuctor_callbacks;
     std::vector<cl_context_properties> m_properties;
